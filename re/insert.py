cb7cd8e66a36fe01c1946f5d58a727526b686dbf
kosyachniy <polozhev@mail.ru>
2018-12-11 16:37:32 +0300

    Fixed bugs

--- re/insert.py
+++ re/insert.py
@@ -5,7 +5,7 @@ import time
 db = sqlite3.connect('db/main.db')
 
 with db:
-	db.execute("INSERT INTO reviews (id, name, cont, time) VALUES (26, 'Имран', 'Нашёл этот обменник, так как тут была самая низкая коммисия. Обменял, перевели не так уж быстро, всё заняло часа два.', (?))", (time.time(),))
+	db.execute("INSERT INTO reviews (id, name, cont, time) VALUES (24, 'Имран', 'Нашёл этот обменник, так как тут была самая низкая коммисия. Обменял, перевели не так уж быстро, всё заняло часа два.', (?))", (time.time(),))
 	db.execute("INSERT INTO reviews (name, cont, time) VALUES ('олололл', 'Обменяли норм', (?))", (time.time(),))
 	db.execute("INSERT INTO reviews (name, cont, time) VALUES ('александр', 'можно было бы и побыстрее', (?))", (time.time(),))
 	db.execute("INSERT INTO reviews (name, cont, time) VALUES ('Corsair', 'Сволочи кинули на 3000 рублей и вату катают', (?))", (time.time(),))
