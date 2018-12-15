199a778564397a4037e61a9d4d838a2863491086
kosyachniy <polozhev@mail.ru>
2018-12-15 16:16:42 +0300

    Clear

--- re/create.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-db = sqlite3.connect('db/main.db')
-
-db.execute("CREATE TABLE history (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(100), mail varchar(100), count real, card varchar(50), course real, referal varchar(100), time timestamp)")
-db.execute("CREATE TABLE support (id INTEGER PRIMARY KEY AUTOINCREMENT, mail varchar(100), cont text, time timestamp)")
-db.execute("CREATE TABLE reviews (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(100), cont text, time timestamp)")
-db.execute("CREATE TABLE news (id INTEGER PRIMARY KEY AUTOINCREMENT, cont text, time timestamp)")
--- re/delete.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-db = sqlite3.connect('db/main.db')
-
-with db:
-	db.execute("DELETE FROM reviews WHERE id=(?)'", (0,))
